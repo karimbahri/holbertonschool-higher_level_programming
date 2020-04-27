487d69ba93723f1b0acfa9c1d1aad96b8d6303b5
karim bahri <karim-bahri-18@outlook.fr>
2020-04-27 21:02:47 +0000

    detect cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,7 +14,7 @@ int check_cycle(listint_t *list)
 	if (!slow || !fast)
 		return (0);
 
-	while (fast && fast->next)
+	while (slow && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
