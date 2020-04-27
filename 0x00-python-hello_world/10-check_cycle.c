f4d95e984a722497d2dccb38c858ec2d1f3cc6a0
karim bahri <karim-bahri-18@outlook.fr>
2020-04-27 20:46:44 +0000

    detect cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - detect loop in linked list
+ * @list: list to check
+ * Return: 1 if loop detected 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	while (slow && fast)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
