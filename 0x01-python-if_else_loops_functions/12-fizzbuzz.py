33d1a88534504d7c61ed78b93b46268b7d5b8dd1
karim bahri <karim-bahri-18@outlook.fr>
2020-04-28 16:55:21 +0000

    fizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,13 @@
 
 
 def fizzbuzz():
-    for i in range(0, 100):
+    for i in range(1, 100):
         if not (i % 3) and not (i % 5):
-            print("FizzBuzz", end=" ")
+            print("FizzBuzz,", end=" ")
         elif not (i % 3):
             print("Fizz", end=" ")
         elif not (i % 5):
-            print("Buzz", end=" ")
+            print("Buzz,", end=" ")
         else:
             print("{:d},".format(i), end=" ")
 
