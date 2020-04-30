c40b480a1795d8dfa6973dd1bbb8d23768526519
karim bahri <karim-bahri-18@outlook.fr>
2020-04-30 13:38:52 +0000

    check req

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import add_0 as opLib
+from add_0 import add
 
 a = 1
 b = 2
 
-print("<a {:d}> + <b {:d}> = <add(a, b) {:d}>".format(a, b, opLib.add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
