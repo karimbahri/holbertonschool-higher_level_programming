e09e0b2bc0a7a69bda799b26eacbd93c73fdb11d
karim bahri <karim-bahri-18@outlook.fr>
2020-05-05 15:28:41 +0000

    multiple return

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+
+    length = len(sentence)
+
+    if not length:
+        first_char = "None"
+    else:
+        first_char = sentence[0]
+
+    return (length, first_char)
