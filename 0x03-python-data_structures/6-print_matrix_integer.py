0038eed389d4933ee25857da2244da570b6d60da
karim bahri <karim-bahri-18@outlook.fr>
2020-05-05 14:27:32 +0000

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for line in matrix:
             for column in line:
-                print("{:d}".format(column), end=" ")
+                print("{:d}".format(column), end="")
+                if column != line[len(line) - 1]:
+                    print(" ", end="")
             print("$")
