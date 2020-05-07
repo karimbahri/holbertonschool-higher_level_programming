80ea7b21369e7fd669208cab4cff2d1f61f7e8be
karim bahri <karim-bahri-18@outlook.fr>
2020-05-07 22:27:42 +0000

    remove at index

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
