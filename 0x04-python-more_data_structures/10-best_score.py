8042114ee8f357d202e1100668a1013e1f97c48d
karim bahri <karim-bahri-18@outlook.fr>
2020-05-07 17:19:21 +0000

    own max

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 
+def max_in_dict(a_dict={}):
+    maxVal = 0
+    maxKey = 0
+    for key, val in a_dict.items():
+        if maxVal < val:
+            maxVal = val
+            maxKey = key
+    return maxKey
+
+
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
     else:
-        maxVal = 0
-        maxKey = 0
-        for key, val in a_dictionary.items():
-            if maxVal < val:
-                maxVal = val
-                maxKey = key
-        return maxKey
+        return max_in_dict(a_dictionary)
