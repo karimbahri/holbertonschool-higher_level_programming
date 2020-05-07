b1f23a64418cc07b33d1ccb1cba0d26bc8733b30
karim bahri <karim-bahri-18@outlook.fr>
2020-05-07 16:47:39 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary is {}:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
-
-    return max(a_dictionary)
+    else:
+        return max(a_dictionary)
