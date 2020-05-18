2bcf2b43007925a22b20531ed5bffb083b83560d
karim bahri <karim-bahri-18@outlook.fr>
2020-05-18 14:52:21 +0000

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
-            print(my_list[i], end='')
-        except:
+            print("{}".format(my_list[i]), end='')
+        except IndexError:
             print('')
             return i
     print('')
