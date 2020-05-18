19fc18e4fc9ee0c62dd4626bc15c6932f192f85c
karim bahri <karim-bahri-18@outlook.fr>
2020-05-18 16:30:02 +0000

    safe div list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,6 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
     res = 0
 
     for i in range(list_length):
+        res = 0
         try:
             res = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
