ca28c783ff44dad6c4070bed375312e732121cd5
karim bahri <karim-bahri-18@outlook.fr>
2020-05-18 15:49:58 +0000

    safe print list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    nb_printed = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except (ValueError, TypeError):
+            continue
+        else:
+            nb_printed += 1
+
+    print('')
+    return nb_printed
