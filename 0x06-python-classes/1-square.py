9df7840b6a0296db32729d77fe5eec6838db38da
karim bahri <karim-bahri-18@outlook.fr>
2020-05-19 14:58:41 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    documentation of the module square
+        *Classes:
+            1 - Square
+"""
+
+
+class Square:
+    """
+    documentation of the class Square
+        *methodes:
+            __init__
+        *attributes:
+            size: private
+    """
+    def __init__(self, s):
+        """documentation of the method __init__ : constructor"""
+        self.__size = s
