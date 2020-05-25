7956077c502ef6130a4df347d8c71581b3b85889
karim bahri <karim-bahri-18@outlook.fr>
2020-05-25 06:38:46 +0000

    empty class: Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+0-rectangle
+    Classes:
+        * Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Rectangle:
+        methodes:
+            * __init__
+    """
+    def __init__(self):
+        """__init__: constructor"""
+        pass
