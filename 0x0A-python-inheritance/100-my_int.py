0dcc43635cb436d7062f8e101b6ce153f29ce459
karim bahri <karim-bahri-18@outlook.fr>
2020-06-02 19:08:53 +0000

    my int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""100-my_int:
+        classes: MyInt
+"""
+
+
+class MyInt(int):
+    """MyInt inherits from int"""
+
+    def __eq__(self, comparator):
+        """eq: not equal function"""
+        result = supper().__int__ != comparator
+        return result
