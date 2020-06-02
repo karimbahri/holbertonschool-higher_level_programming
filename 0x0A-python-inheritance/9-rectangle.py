9aa927622f474a663f508110d550162dc31262f7
karim bahri <karim-bahri-18@outlook.fr>
2020-06-02 18:48:17 +0000

    rectangle doc fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
 """
     5-base_geometry
         Classes:
             Rectangle
 """
-
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """BaseGeometry: empty class"""
