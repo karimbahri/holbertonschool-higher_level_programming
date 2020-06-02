a4c9ff1131f214b5f80eb140ed82153e8ed165ec
karim bahri <karim-bahri-18@outlook.fr>
2020-06-02 14:23:14 +0000

    is instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+2-is_same_class
+    functions:
+        is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """check if obj is instance of a_class"""
+    return isinstance(obj, a_class)
