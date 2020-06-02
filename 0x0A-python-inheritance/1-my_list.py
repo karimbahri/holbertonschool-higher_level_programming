dd1a9764bcfd2d3897dd105f6a0746ce372c780b
karim bahri <karim-bahri-18@outlook.fr>
2020-06-02 13:54:40 +0000

    sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    my_list:
+        classes:
+            MyList
+"""
+
+
+class MyList(list):
+    """MyList: inherits from list"""
+
+
+    def print_sorted(self):
+        """print_sorted: ascending sort"""
+
+        print(sorted(self))
