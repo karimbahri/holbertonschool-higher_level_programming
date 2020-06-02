df98e051f2653f2ce08c645a78faeb48a7c62151
karim bahri <karim-bahri-18@outlook.fr>
2020-06-02 18:07:31 +0000

    square str

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+Rectangle = __import__("9-rectangle").Rectangle
+"""
+10-square:
+    classes:
+        Square
+"""
+
+
+class Square(Rectangle):
+    """Square: inherits from rectangle"""
+    def __init__(self, size):
+        """constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """
+        str: string representation of the object
+        """
+        string = "[Square] {}/{}".format(self.__size, self.__size)
+        return string
