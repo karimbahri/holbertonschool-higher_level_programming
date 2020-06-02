33b875b114d0e3fb747cffb6fee03eec3186e344
karim bahri <karim-bahri-18@outlook.fr>
2020-06-02 15:50:25 +0000

    base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    5-base_geometry
+        Classes:
+            BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry: empty class"""
+
+    def area(self):
+        """area: raise exception"""
+        raise Exception("area() is not implemented")
