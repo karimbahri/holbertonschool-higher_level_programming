9b3f69f739bd2f5b7d7efd8b95c4d50950e0c2e4
karim bahri <karim-bahri-18@outlook.fr>
2020-06-02 17:25:06 +0000

    str n area impl

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""
+    5-base_geometry
+        Classes:
+            Rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """BaseGeometry: empty class"""
+    def __init__(self, width, height):
+        """constructor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """area: return area"""
+        return self.__width * self.__height
+
+    def integer_validator(self, name, value):
+        """integer_validator: checks"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+    def __str__(self):
+        """str: string representation of rectangle instance"""
+        string = "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return string
