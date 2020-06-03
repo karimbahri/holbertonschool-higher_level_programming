32b8b84c8f054540c70da219ad9e330e8c0da7af
karim bahri <karim-bahri-18@outlook.fr>
2020-06-03 21:58:23 +0000

    save json file

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """save to jason file"""
     with open(filename, "w") as my_file:
-        my_file.write(json.dumps(obj))
+        my_file.write(json.dumps(my_obj))
