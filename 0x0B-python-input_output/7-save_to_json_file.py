6dd19338679385e103c4e12f39e4eda0f107d04e
karim bahri <karim-bahri-18@outlook.fr>
2020-06-03 21:56:21 +0000

    save json file

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""7-save_to_jason_file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save to jason file"""
+    with open(filename, "w") as my_file:
+        my_file.write(json.dumps(obj))
