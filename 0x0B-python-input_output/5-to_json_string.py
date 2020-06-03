60145ff6413c54e6761d1a97959149541f5e9fbe
karim bahri <karim-bahri-18@outlook.fr>
2020-06-03 21:20:05 +0000

    jason repr

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+5-to_json
+    functions:
+        to_json_string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """to_json_string: return jason repr of my_obj"""
+    json_obj = json.dump()
+    return json_obj
