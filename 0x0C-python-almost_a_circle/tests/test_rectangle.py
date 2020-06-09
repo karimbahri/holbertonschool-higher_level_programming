33e841ac5d60062c9e32b5a62a9bd4223f255cb1
karim bahri <karim-bahri-18@outlook.fr>
2020-06-09 21:00:40 +0000

    rect test

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -10,3 +10,12 @@ class test_rectangle(unittest.TestCase):
         """function test"""
         rect = Rectangle(5, 4)
         self.assertEqual(rect.width, 5)
+        self.assertEqual(rect.height, 4)
+        self.assertEqual(rect.id, 3)
+        self.assertEqual(rect.x, 0)
+        self.assertEqual(rect.y, 0)
+
+        rect1 = Rectangle(5, 4, 10, 19, 44)
+        self.assertEqual(rect1.x, 10)
+        self.assertEqual(rect1.y, 19)
+        self.assertEqual(rect1.id, 44)
