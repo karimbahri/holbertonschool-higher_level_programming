21ba111faf7bbce7dff3fdb41d6713c5f9bc600b
karim bahri <karim-bahri-18@outlook.fr>
2020-07-13 17:32:03 +0000

    best score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- best_score
 
 -- sort table where score >= 10
-SELECT id, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
