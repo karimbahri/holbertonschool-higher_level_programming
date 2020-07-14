fa5e346c842fd0837744377a2b53c9b9e244f441
karim bahri <karim-bahri-18@outlook.fr>
2020-07-14 15:29:51 +0000

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,6 @@
 -- privileges
 
 --  all privileges of the MySQL users user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
 --  all privileges of the MySQL users user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
