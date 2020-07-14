2ad8f22bb3552041073be59529d5460a109aaf33
karim bahri <karim-bahri-18@outlook.fr>
2020-07-14 17:52:30 +0000

    citiesByStates

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- city_by_state
 
 -- lists all cities
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.states_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
