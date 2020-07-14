d75de9b80cd21972b4b10ae1a68cbf43e0a4095f
karim bahri <karim-bahri-18@outlook.fr>
2020-07-14 20:36:15 +0000

    no_genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,4 @@
+-- no_genre
+
+-- lists all shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id IS NULL ORDER BY title ASC, genre_id;
