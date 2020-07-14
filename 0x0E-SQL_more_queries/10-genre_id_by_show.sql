e745879da7870ab04ade1d3d16a722ae65a08bb6
karim bahri <karim-bahri-18@outlook.fr>
2020-07-14 19:10:31 +0000

    tvShow genre

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- genre_id_by_show
+
+-- lists all shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
