49a0c743a25ec78a718f2915705fea7e67aee899
karim bahri <karim-bahri-18@outlook.fr>
2020-08-08 23:11:53 +0000

    select order by name

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+print states from database
+"""
+from sys import argv
+import MySQLdb
+
+dataBase = MySQLdb.connect(host="localhost", port="3306", user=argv[1],
+                           passwd=argv[2], db=argv[3])
+
+cursor = dataBase.cursor()
+
+cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+
+rows = cursor.fetchall()
+
+for element in rows:
+    print(element)
+
+cursor.close()
+dataBase.close()
