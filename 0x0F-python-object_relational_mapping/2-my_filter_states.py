2a59a4f84472b76bf50afdf069efec605cfa3060
karim bahri <karim-bahri-18@outlook.fr>
2020-08-09 12:40:23 +0000

    filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+displays all values in the states
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    dataBase = MySQLdb.connect(host="localhost",
+                               port=3306, user=argv[1],
+                               passwd=argv[2], db=argv[3])
+
+    cursor = dataBase.cursor()
+
+    cursor.execute("SELECT * FROM states\
+                   WHERE name LIKE BINARY\
+                   '{}' ORDER BY\
+                   states.id ASC"
+                   .format(argv[4]))
+
+    cursor.fetchall()
+
+    for element in cursor:
+        print(element)
+
+    cursor.close()
+    dataBase.close()
