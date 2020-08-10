3ebb9a21fcc46bd883d9dfdfdc9bc0ae91de5fbd
karim bahri <karim-bahri-18@outlook.fr>
2020-08-10 13:21:54 +0000

    model state

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""list all objects from DB"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://\
+                          {}:{}@localhost/{}'.format(sys.argv[1],
+                                                     sys.argv[2],
+                                                     sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    sess = Session()
+    for element in sess.query(element).order_by(asc(element.id)):
+        print("{}: {}".format(element.id, element.name))
+    sess.close()
