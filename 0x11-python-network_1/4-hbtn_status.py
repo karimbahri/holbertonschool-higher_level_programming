d5b6a438f6f8fa8a2fae4ab1cd04915ca9cafdda
karim bahri <karim-bahri-18@outlook.fr>
2020-08-24 17:16:08 +0000

    request

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""fetch url"""
+if __name__ == "__main__":
+    import requests
+    print("Body response:")
+    req = requests['https://intranet.hbtn.io/status']
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content {}".format(req.text))
