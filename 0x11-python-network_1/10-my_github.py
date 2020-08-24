4b54c6fda8ac2c53b93adf8f2bf2f2cdfeefc0a4
karim bahri <karim-bahri-18@outlook.fr>
2020-08-24 19:53:48 +0000

    github access token

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""github id"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    link = "https://api.github.com/user"
+    user = argv[1]
+    access_token = argv[2]
+    req = requests.get(link, auth=HTTPBasicAuth(user, access_token))
+    print(req.json()['id'])
