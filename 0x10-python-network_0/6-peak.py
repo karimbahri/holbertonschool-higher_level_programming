30efae1b013732c5b66ec90fb60d1b54488d2876
karim bahri <karim-bahri-18@outlook.fr>
2020-08-24 21:57:21 +0000

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,7 +5,7 @@
 def find_peak(list_of_integers):
     """find peak in unsorted list"""
     ls = list_of_integers
-    if not ls or not isinstance(ls, list):
+    if not ls or ls == []:
         return None
     for i in range(1, len(ls) - 1):
         if ls[i] > ls[i - 1] and ls[i + 1] < ls[i]:
