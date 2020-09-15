ec1d07b5fc6b9d23d54e389fcae9e45c35bdd0cc
karim bahri <karim-bahri-18@outlook.fr>
2020-09-15 16:35:50 +0100

    multi-line

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const text1 = 'C is fun';
+const text2 = 'Python is cool';
+const text3 = 'Javascript is amazing';
+
+console.log(text1);
+console.log(text2);
+console.log(text3);
