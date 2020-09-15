7ebed4b5635efb6a3d54b373ca18b942b8f124e1
karim bahri <karim-bahri-18@outlook.fr>
2020-09-15 17:55:48 +0100

    argv value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,5 @@
 if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  process.argv.forEach(element => {
-    console.log(`${element}`);
-  });
+  console.log(process.argv[2]);
 }
