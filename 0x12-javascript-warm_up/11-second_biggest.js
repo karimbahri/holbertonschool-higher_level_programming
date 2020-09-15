357350bc58b6a2d4fbe4d00aada289a399365f10
karim bahri <karim-bahri-18@outlook.fr>
2020-09-15 21:34:26 +0100

    second max

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const len = process.argv.length;
+
+let max1 = 0;
+let max2 = 0;
+
+for (let i = 0; i < len; i++) {
+  if (Number(process.argv[i]) > max1) {
+    max2 = max1;
+    max1 = process.argv[i];
+  }
+}
+console.log(max2);
