d8ecd97feb46683f1effe4a9c37a001adc95a709
karim bahri <karim-bahri-18@outlook.fr>
2020-09-16 10:17:43 +0100

    fix second max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -9,6 +9,8 @@ for (let i = 0; i < len; i++) {
   if (Number(process.argv[i]) > max1) {
     max2 = max1;
     max1 = process.argv[i];
+  } else if (Number(process.argv[i]) > max2) {
+    max2 = process.argv[i];
   }
 }
 console.log(max2);
