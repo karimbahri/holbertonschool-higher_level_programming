6e23ab6d7e71891b688abb6505702f8f36f63611
karim bahri <karim-bahri-18@outlook.fr>
2020-09-22 15:39:02 +0100

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (e, d) {
+  if (e) {
+    console.log(e);
+  } else {
+    console.log('code: ', d.statusCode);
+  }
+});
