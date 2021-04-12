b696c64e80891f9fdb43c3de763058901e727041
karim bahri <karim-bahri-18@outlook.fr>
2021-04-12 03:14:02 +0100

    14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,4 @@
 SELECT tv_genres.name FROM tv_show
 JOIN tv_show_genres ON tv_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+ORDER BY tv_genres.name;
