916d4cc0e38aaf89a0fbe64750d1cc986b674996
karim bahri <karim-bahri-18@outlook.fr>
2021-04-12 03:07:16 +0100

    13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+
+SELECT tv_genres.name As genre , COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_show_genres JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
